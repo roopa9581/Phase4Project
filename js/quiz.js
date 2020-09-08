6cda7cb4f9009a266d9877f5e937ed50f40912fe
roopa9581 <mroopa7879@gmail.com>
2020-09-08 12:59:10 +0530

    First commit

---
+++ js/quiz.js
@@ -0,0 +1,54 @@
+let quiz=[
+    {
+        question:"HTML stands for?",
+        option:[
+            "Hyper Text Markup Language",
+            "High Text Markup Language",
+            "Hyper Tabular Markup Language",
+            "None of these",
+        ],
+        answer:1,
+    },
+    {
+        question:" which of the following tag is used to mark a begining of paragraph ?",
+        option:[
+            "<TD>",
+            "<br>",
+            "<P>",
+           "<TR>",
+            
+        ],
+        answer:3,
+    },
+    {
+        question:"From which tag descriptive list starts ?",
+        option:[
+            "<LL>",
+            "<DD>",
+            "<DL>",
+           "<DS>",   
+        ],
+        answer:3,
+    },
+    {
+        question:"Correct HTML tag for the largest heading is",
+        option:[
+            "<head>",
+            "<h6>",
+            "<heading>",
+            "<h1>",   
+        ],
+        answer:4,
+
+    },
+    {
+        question:"The attribute of<form> tag",
+        option:[
+            "Method",
+            "Action",
+            "Both 1 and 2",
+            "Non of these",   
+        ],
+        answer:3,
+    }
+]
